ae8b065ea441fe883e556391bee70a700a674397
adam-algatt <adam.algatt@gmail.com>
2022-02-18 18:04:36 -0800

    pass param confirm and push outside of param function'

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,28 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById('generate');
 
+
+var passParameters = ["lowercase", "uppercase"] // "numeric", "special"];
+let paramArr = [];
+function getParameters() {
+for (i = 0; i < passParameters.length; i++) {
+  let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
+  //console.log(passParameters[i]);
+ 
+  if (prompt === true) {
+    paramArr.push(passParameters[i])
+  } else {
+    break;
+  }
+}
+return paramArr; 
+
+}
+getParameters(); 
+console.log(paramArr.length);
+/*
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +35,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
+
+
+
+// Random # syntax Math.floor((Math.random() * passLength) + 1);
