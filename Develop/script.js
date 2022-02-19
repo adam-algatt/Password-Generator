4a654bd3f86d29023a6b40c571a92a6c64879fbd
adam-algatt <adam.algatt@gmail.com>
2022-02-19 11:57:32 -0800

    code loops back through pass parameters if user doesn't select any

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,45 @@
-// Assignment code here
+
 
 
 // Get references to the #generate element
 var generateBtn = document.getElementById('generate');
+var passParameters = ["lowercase", "uppercase", "numeric", "special"];
+var paramArr = [];
+var passLength = prompt('How long would you like your password to be?', "Please enter a number between 8 and 128");
+var count = 0;
 
 
-var passParameters = ["lowercase", "uppercase"] // "numeric", "special"];
-let paramArr = [];
-function getParameters() {
-for (i = 0; i < passParameters.length; i++) {
-  let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
-  //console.log(passParameters[i]);
- 
-  if (prompt === true) {
-    paramArr.push(passParameters[i])
-  } else {
-    break;
+
+//function confirms password parameters from user
+let getParams = function() {
+  for (i = 0; i < passParameters.length; i++) {
+    let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
+   count++;   
+    //console.log(passParameters[i]);
+    if (prompt === true) {
+      paramArr.push(passParameters[i]);
+    } else {
+
+    }
   }
+  
+  //prevents user from selecting a password with no character
+  //types
+  if (paramArr.length === 1 && count === 4) {
+ "Select at least one pass variable."
+ count = 0; 
+ getParams();
+  }
+  return paramArr;
+  return count; 
 }
-return paramArr; 
-
-}
-getParameters(); 
+if (passLength >= 8 && passLength <= 128 && paramArr.length === 0) {
+	paramArr.push(passLength); 
+	getParams(); 
+} 
+//8=getParameters();
 console.log(paramArr.length);
+
 /*
 // Write password to the #password input
 function writePassword() {
