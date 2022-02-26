4be8db6f5bb30e3b9f17561d6ab2b15cdad0a600
adam-algatt <adam.algatt@gmail.com>
2022-02-26 14:04:06 -0800

    get length and populate parameter functions working correctly

--- Develop/script.js
+++ Develop/script.js
@@ -7,27 +7,46 @@ var passLength = 0;
 var count = 0;
 var whatLength = "How long would you like your password to be?";
 
+var lowercase = function() {
+console.log('1');
+};
+
+var uppercase = function() {
+  console.log('2');
+};
+
+var numeric = function() {
+  console.log('3');
+};
+
+var special = function() {
+  console.log('4');
+};
+
+
 var randomCharCallback = {
-  lowercase: lowercase(),
-  uppercase: uppercase(),
-  numeric: numeric(),
-  special: special(),
+  lowercase: lowercase,
+  uppercase: uppercase,
+  numeric: numeric,
+  special: special
 };
 
 
 //Calls appropriate function in randomParamGenerator to generate random character
-var generatePassword = function () {
-  for (i = 1; i < passLength; i++) {
+var generatePassword = function (int) {
+  console.log('why god why?!');
+  for (i = 0; i < int; i++) {
     //get random number 1-4 to call pass param
-    var randomParam = passParameters[Math.floor((Math.random() * 4))];
+    var randomNumberCeiling = (paramArr.length - 1);
+    var randomParam = passParameters[Math.floor(Math.random() * randomNumberCeiling)];
     var callback = randomCharCallback.randomParam;
-    return callback;
+    console.log(callback);
   }
+  return callback;
 };
 
 
 var populateParameters = function () {
-
   for (i = 0; i < passParameters.length; i++) {
     let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
     count++;
@@ -38,24 +57,38 @@ var populateParameters = function () {
 
     }
   }
-    return paramArr;
-    return count;
-    return passLength;
-    generatePassword();
+  //returns count to zero and initiates populateParameter function if user
+  //doesn't select any parameters for their password
+  if (count === 4 && paramArr.length === 0) {
+  count = 0; 
+  alert('Please select at least one parameter for your password');
+  populateParameters(); 
+}
+  else {
+    
+  }
+  console.log(paramArr);
+  console.log(count);
+  console.log(passLength);
+  generatePassword(passLength);
+  return paramArr;
 };
 
+//Gets length of password and calls parameter function 
 var getLength = function () {
-  prompt(whatLength);
-  if (typeof (prompt) !== number) {
+  var lengthPrompt = prompt(whatLength);
+  console.log(lengthPrompt);
+  if (isNaN(lengthPrompt) === true) {
     alert('Please enter only NUMBERS.')
     getLength();
-  } else if (prompt < 8 || prompt > 128) {
+  } else if (lengthPrompt < 8 || lengthPrompt > 128) {
     alert('Please enter a length between 8 and 128');
     getLength();
   } else {
-    passLength = prompt;
-    populateParameters();
+ 
   }
+  passLength = lengthPrompt; 
+  populateParameters();
   return passLength;
 };
 
