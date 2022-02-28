8a6891aec97849ace2a18c2be3f49392e003b261
adam-algatt <adam.algatt@gmail.com>
2022-02-28 15:54:57 -0800

    password generates correctly

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,65 @@
 // Get references to the #generate element
 var generateBtn = document.getElementById('generate');
-var password = document.getElementById('password');
+var passwordel = document.getElementById('password');
 var passParameters = ["lowercase", "uppercase", "numeric", "special"];
 var paramArr = [];
 var passLength = 0;
 var count = 0;
 var whatLength = "How long would you like your password to be?";
-var generatedPassword = [];
+var generatedPassword = "";
 
- function lowercase() {
-  generatedPassword.push(String.fromCharCode(Math.random() * (123 - 97) + 97));
-  return generatedPassword;
-};
+// function lowercase() {
+//   var lowerRand = String.fromCharCode(Math.random() * (123 - 97) + 97);
+//   return lowerRand;
+// };
 
- function uppercase() {
-  generatedPassword.push(String.fromCharCode(Math.random() * (91 - 65) + 65));
-  return generatedPassword;
-};
+// function uppercase() {
+//   var upperRand = String.fromCharCode(Math.random() * (91 - 65) + 65);
+//   return upperRand;
+// };
 
- function numeric() {
-  generatedPassword.push(String.fromCharCode(Math.random() * (58 - 48) + 48));
-  return generatedPassword;
-};
+// function numeric() {
+//   var numRand =  String.fromCharCode(Math.random() * (58 - 48) + 48); 
+//   return numRand;
+// };
 
-function special() {
-  generatedPassword.push(String.fromCharCode(Math.random() * (48 - 33) + 33));
-  return generatedPassword;
-};
+// function special() {
+//   var specRand = String.fromCharCode(Math.random() * (48 - 33) + 33);
+//   return specRand;
+// };
+// console.log(generatedPassword);
 
 
-var randomCharCallback = {
-  lowercase: lowercase(),
-  uppercase: uppercase(),
-  numeric: numeric(),
-  special: special()
-};
 
 
 //Calls appropriate function in randomParamGenerator to generate random character
-var generatePassword = function () {
+var generatePassword = function (num) {
   console.log('why god why?!');
-  for (i = 0; i < 12; i++) {
+  var password = "";
+
+  for (i = 0; i < num; i++) {
     //get random number 1-4 to call pass param
-    var randomNumberCeiling = (paramArr.length - 1);
-    // console.log(typeof(randomParam));
-    randomCharCallback[passParameters[Math.floor(Math.random() * randomNumberCeiling)]]; 
-    // console.log(callRandom);
-    
-  }
-  console.log('password:', generatedPassword);
-  
+    var randomNumberCeiling = paramArr.length;
+     var passChar = paramArr[Math.floor(Math.random() * randomNumberCeiling)];
+    // password += randomArr[passChar];
+    if (passChar === 'lowercase'){
+    password += String.fromCharCode(Math.random() * (123 - 97) + 97);
+    }else if (passChar === 'uppercase'){
+      password += String.fromCharCode(Math.random() * (91 - 65) + 65);
+    } else if (passChar === 'numeric'){
+      password += String.fromCharCode(Math.random() * (58 - 48) + 48);
+    } else if (passChar === 'special') {
+      password += String.fromCharCode(Math.random() * (48 - 33) + 33);
+  } else {}
+
 };
+console.log('password:', password);
+return password;
+}
 
 
 var populateParameters = function () {
+  paramArr = [];
   for (i = 0; i < passParameters.length; i++) {
     let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
     count++;
@@ -67,40 +73,37 @@ var populateParameters = function () {
   //returns count to zero and initiates populateParameter function if user
   //doesn't select any parameters for their password
   if (count === 4 && paramArr.length === 0) {
-  count = 0; 
-  alert('Please select at least one parameter for your password');
-  populateParameters(); 
-}
-  else {
-    
+    count = 0;
+    alert('Please select at least one parameter for your password');
+    populateParameters();
+  } else {
+
   }
-  console.log(paramArr);
-  console.log(count);
+  console.log('paramArr:', paramArr);
+  // console.log(count);
   console.log('passlength:', passLength);
-  generatePassword(passLength);
   return paramArr;
 };
 
-//Gets length of password and calls parameter function 
-var getLength = function () {
+generateBtn.addEventListener("click", function (){
+ // gets user password length
   var lengthPrompt = prompt(whatLength);
   console.log(lengthPrompt);
+  // if prompt returns NaN or outside min/max range, trigger another prompt
   if (isNaN(lengthPrompt) === true) {
     alert('Please enter only NUMBERS.')
-    getLength();
+    prompt(whatLength);
   } else if (lengthPrompt < 8 || lengthPrompt > 128) {
     alert('Please enter a length between 8 and 128');
-    getLength();
+    prompt(whatLength);
   } else {
- 
+
   }
-  passLength = parseInt(lengthPrompt); 
+  //changes prompt val from string to number
+  passLength = parseInt(lengthPrompt);
   populateParameters();
-  return passLength;
-};
-
-
-generateBtn.addEventListener("click", getLength);
+  generatePassword(passLength); 
+});
 
 
 
@@ -117,4 +120,3 @@ generateBtn.addEventListener("click", getLength);
 //  Random # syntax Math.floor((Math.random() * passLength) + 1);
 // var password = generatePassword();
 // var passwordText = document.querySelector("#password");
-// 
