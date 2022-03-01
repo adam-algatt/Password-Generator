1be30a537085d86d832777ffbf1ce85d2a10e0d8
adam-algatt <adam.algatt@gmail.com>
2022-02-28 16:15:09 -0800

    cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Get references to the #generate element
+//Global Variables
 var generateBtn = document.getElementById('generate');
 var passwordel = document.getElementById('password');
 var passParameters = ["lowercase", "uppercase", "numeric", "special"];
@@ -8,58 +8,38 @@ var count = 0;
 var whatLength = "How long would you like your password to be?";
 var generatedPassword = "";
 
-// function lowercase() {
-//   var lowerRand = String.fromCharCode(Math.random() * (123 - 97) + 97);
-//   return lowerRand;
-// };
-
-// function uppercase() {
-//   var upperRand = String.fromCharCode(Math.random() * (91 - 65) + 65);
-//   return upperRand;
-// };
-
-// function numeric() {
-//   var numRand =  String.fromCharCode(Math.random() * (58 - 48) + 48); 
-//   return numRand;
-// };
-
-// function special() {
-//   var specRand = String.fromCharCode(Math.random() * (48 - 33) + 33);
-//   return specRand;
-// };
-// console.log(generatedPassword);
-
-
-
-
 //Calls appropriate function in randomParamGenerator to generate random character
 var generatePassword = function (num) {
   console.log('why god why?!');
   var password = "";
-
+  //generates a char based on user-selected parameters, num is the password length
   for (i = 0; i < num; i++) {
     //get random number 1-4 to call pass param
     var randomNumberCeiling = paramArr.length;
-     var passChar = paramArr[Math.floor(Math.random() * randomNumberCeiling)];
-    // password += randomArr[passChar];
-    if (passChar === 'lowercase'){
-    password += String.fromCharCode(Math.random() * (123 - 97) + 97);
-    }else if (passChar === 'uppercase'){
+    var passChar = paramArr[Math.floor(Math.random() * randomNumberCeiling)];
+
+    if (passChar === 'lowercase') {
+      password += String.fromCharCode(Math.random() * (123 - 97) + 97);
+    } else if (passChar === 'uppercase') {
       password += String.fromCharCode(Math.random() * (91 - 65) + 65);
-    } else if (passChar === 'numeric'){
+    } else if (passChar === 'numeric') {
       password += String.fromCharCode(Math.random() * (58 - 48) + 48);
     } else if (passChar === 'special') {
       password += String.fromCharCode(Math.random() * (48 - 33) + 33);
-  } else {}
+    } else {}
 
-};
-console.log('password:', password);
-return password;
+  };
+  //adds generated password to passwordel text
+  passwordel.textContent = password;
+  //appends passwordel children
+  passwordel.appendChild();
+  return password;
 }
 
-
+//populates paramArr with user selected character types
 var populateParameters = function () {
   paramArr = [];
+  //iterates over char type options for user and populates, paramArr with selected options
   for (i = 0; i < passParameters.length; i++) {
     let prompt = confirm('Would you like to use ' + passParameters[i] + ' characters in your password?');
     count++;
@@ -79,14 +59,11 @@ var populateParameters = function () {
   } else {
 
   }
-  console.log('paramArr:', paramArr);
-  // console.log(count);
-  console.log('passlength:', passLength);
   return paramArr;
 };
 
-generateBtn.addEventListener("click", function (){
- // gets user password length
+generateBtn.addEventListener("click", function () {
+  // gets user password length
   var lengthPrompt = prompt(whatLength);
   console.log(lengthPrompt);
   // if prompt returns NaN or outside min/max range, trigger another prompt
@@ -102,21 +79,5 @@ generateBtn.addEventListener("click", function (){
   //changes prompt val from string to number
   passLength = parseInt(lengthPrompt);
   populateParameters();
-  generatePassword(passLength); 
+  generatePassword(passLength);
 });
-
-
-
-
-
-//  Write password to the #password input
-// function writePassword(arr) {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }  
-//  Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);  
-//  Random # syntax Math.floor((Math.random() * passLength) + 1);
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
--- README.md
+++ README.md
@@ -1,38 +1,5 @@
-# Password Generator Starter Code
+#Password Generator 
 
-
-##Acceptance Criteria
-
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-
-//  Write password to the #password input
-// function writePassword(arr) {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }  
-//  Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);  
-//  Random # syntax Math.floor((Math.random() * passLength) + 1);
-// var password = generatePassword();
-// var passwordText = document.querySelector("#password");
-// 
+##Generates User-Defined Password
+When 'Generate Password, is clicked the user is presented a series of prompts giving them the options to choose password:
+length, and character types. After the user answers all the prompts a password is generated and added to the '#password' text area. 
